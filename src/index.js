4ba5202c597db55256acee90b767f5114ae02b91
Hostess27 <pash.pash22173@gmail.com>
2021-10-14 21:30:12 +0300

    Финальные правки перед сдачей дз 10

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-import './sass/main.scss';
+import theme from './js/theme';
+import menuTemplate from './templates/menu.hbs';
+import menu from './templates/menu.json';
 import './styles.css';
+
+const refs = {
+    input: document.querySelector('.theme-switch__toggle'),
+    menu: document.querySelector('.js-menu'),
+    body: document.querySelector('body'),
+  };
+  
+  if (localStorage.getItem('theme') === theme.DARK) {
+    refs.input.checked = true;
+  }
+  
+  const changeBodyCLass = () => {
+    refs.body.classList.add(localStorage.getItem('theme'));
+  };
+  const changeLocalStorage = (set, del) => {
+    localStorage.setItem('theme', set);
+    changeBodyCLass();
+    refs.body.classList.remove(del);
+  };
+  const themeSwitcher = () => {
+    if (refs.input.checked) {
+      changeLocalStorage(theme.DARK, theme.LIGHT);
+    } else {
+      changeLocalStorage(theme.LIGHT, theme.DARK);
+    }
+  };
+  
+  changeBodyCLass();
+  refs.input.addEventListener('change', themeSwitcher);
+  
+  const markup = menuTemplate(menu);
+  refs.menu.insertAdjacentHTML('beforeend', markup);
+
+
+
+
