be827780e7bb1141fc9930d978cb556ae64c98cd
Hostess27 <pash.pash22173@gmail.com>
2021-10-15 18:21:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,38 +3,37 @@ import menuTemplate from './templates/menu.hbs';
 import menu from './templates/menu.json';
 import './styles.css';
 
+
 const refs = {
-    input: document.querySelector('.theme-switch__toggle'),
-    menu: document.querySelector('.js-menu'),
-    body: document.querySelector('body'),
-  };
-  
-  if (localStorage.getItem('theme') === theme.DARK) {
-    refs.input.checked = true;
-  }
-  
-  const changeBodyCLass = () => {
-    refs.body.classList.add(localStorage.getItem('theme'));
-  };
-  const changeLocalStorage = (set, del) => {
-    localStorage.setItem('theme', set);
-    changeBodyCLass();
-    refs.body.classList.remove(del);
-  };
-  const themeSwitcher = () => {
-    if (refs.input.checked) {
-      changeLocalStorage(theme.DARK, theme.LIGHT);
-    } else {
-      changeLocalStorage(theme.LIGHT, theme.DARK);
-    }
-  };
-  
+  input: document.querySelector('.theme-switch__toggle'),
+  menu: document.querySelector('.js-menu'),
+  body: document.querySelector('body'),
+};
+
+if (localStorage.getItem('theme') === theme.DARK) {
+  refs.input.checked = true;
+} else {
+  refs.body.classList.add(theme.LIGHT);
+}
+
+const changeBodyCLass = () => {
+  refs.body.classList.add(localStorage.getItem('theme'));
+};
+const changeLocalStorage = (set, del) => {
+  localStorage.setItem('theme', set);
   changeBodyCLass();
-  refs.input.addEventListener('change', themeSwitcher);
-  
-  const markup = menuTemplate(menu);
-  refs.menu.insertAdjacentHTML('beforeend', markup);
+  refs.body.classList.remove(del);
+};
+const themeSwitcher = () => {
+  if (refs.input.checked) {
+    changeLocalStorage(theme.DARK, theme.LIGHT);
+  } else {
+    changeLocalStorage(theme.LIGHT, theme.DARK);
+  }
+};
 
+changeBodyCLass();
+refs.input.addEventListener('change', themeSwitcher);
 
-
-
+const markup = menuTemplate(menu);
+refs.menu.insertAdjacentHTML('beforeend', markup);
